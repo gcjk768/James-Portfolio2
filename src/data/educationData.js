d50198d45da3bf0dc0fbfedb0f1d7f0a08101ded
gcjk768 <james.kohgc@gmail.com>
2022-09-23 17:42:10 +0800

    Second update

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,7 +1,7 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'University of Wollongong (SIM Global Education)',
+        institution: 'University of Wollongong (SIM Global Education, Singapore)',
         course: 'Bachelor of Computer Science (Game and Mobile Development)',
         startYear: '2021',
         endYear: '2023 (Expected)'
